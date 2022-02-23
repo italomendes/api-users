df8ea623cdc946c8d6f5632957f66452616982a3
Italo Mendes <italomemendes@gmail.com>
2022-02-23 10:55:53 -0300

    refactor: delete unused files

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,13 +1,11 @@
 import costCenterRouter from '@modules/cost_center/routes/CostCenter.routes';
 import departmentRouter from '@modules/department/routes/Department.routes';
-import productsRouter from '@modules/products/routes/products.routes';
 import roleRouter from '@modules/role/routes/Role.routes';
 import userRouter from '@modules/user/routes/User.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
-//routes.use('/products', productsRouter);
 routes.use('/cost-center', costCenterRouter);
 routes.use('/department', departmentRouter);
 routes.use('/role', roleRouter);
