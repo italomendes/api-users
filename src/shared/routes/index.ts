409ba987ec4a919e0b8d9038cb7593d29152efbe
Italo Mendes <italomemendes@gmail.com>
2022-02-23 10:26:20 -0300

    feat: add routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,6 +1,8 @@
 import costCenterRouter from '@modules/cost_center/routes/CostCenter.routes';
 import departmentRouter from '@modules/department/routes/Department.routes';
 import productsRouter from '@modules/products/routes/products.routes';
+import roleRouter from '@modules/role/routes/Role.routes';
+import userRouter from '@modules/user/routes/User.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -8,5 +10,7 @@ const routes = Router();
 //routes.use('/products', productsRouter);
 routes.use('/cost-center', costCenterRouter);
 routes.use('/department', departmentRouter);
+routes.use('/role', roleRouter);
+routes.use('/user', userRouter);
 
 export default routes;
